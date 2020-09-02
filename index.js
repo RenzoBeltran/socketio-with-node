13fcb568e84c2eea680490cb2632354cd002c51c
RenzoBeltran <renzob130@gmail.com>
2020-09-02 17:28:33 -0500

    ADD: first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require("express");
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//start the server
+const server = app.listen(app.get('port'), () => {
+ console.log('server on port', app.get('port'));
+})
+
+//websockets
+const SocketIO = require("socket.io");
+const io = SocketIO(server);
+
+io.on("connection", (socket) => {
+ socket.on('chat:message', (data) => {
+  io.sockets.emit('chat:message', data)
+ })
+ socket.on('chat:typing', (username) => {
+  socket.broadcast.emit('chat:typing', username)
+ })
+
+})
